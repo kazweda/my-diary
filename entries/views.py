2daa3ad4c7feaeb7e1208377a7f3a44cca57751b
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2024-07-29 16:49:55 +0900

    fix #5

--- entries/views.py
+++ entries/views.py
@@ -1,6 +1,10 @@
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
 )
 
 from .models import Entry
@@ -13,3 +17,22 @@ class EntryListView(ListView):
 
 class EntryDetailView(DetailView):
     model = Entry
+
+
+class EntryCreateView(CreateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_url = reverse_lazy("entry-list")
+
+
+class EntryUpdateView(UpdateView):
+    model = Entry
+    fields = ["title", "content"]
+
+    def get_success_url(self):
+        return reverse_lazy("entry-detail", kwargs={"pk": self.entry.id})
+
+
+class EntryDeleteView(DeleteView):
+    model = Entry
+    success_url = reverse_lazy("entry-list")
